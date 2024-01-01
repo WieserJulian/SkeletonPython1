1ccbef43f64ab1b99e8f78ae3fff38083cdfcade
WieserJulian <julian.wieser@i-wieser.com>
2024-01-01 16:12:00 +0100

    Update As 9 adapt 7

--- Assignment_7/test_Assignment_7.py
+++ Assignment_7/test_Assignment_7.py
@@ -19,15 +19,17 @@ class test_Assignment_7(unittest.TestCase):
             from a7_ex1 import Radian
             c = Radian(90)
             c.degree
-            self.assertAlmostEqual(1.57, c.rad(), places=2)
-            solve_text = "The degree is 90.00 and the radian is 1.57\n"
-            with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as stdout:
-                c.print()
-            self.assertEqual(solve_text, stdout.getvalue())
-
         except (AttributeError, ImportError, ModuleNotFoundError) as er:
             warnings.warn("You need to put the a7_ex1.py in the same directory",ImportWarning, source=er)
             self.skipTest("Possible not implemented")
+
+        from a7_ex1 import Radian
+        c = Radian(90)
+        self.assertAlmostEqual(1.57, c.rad(), places=2)
+        solve_text = "The degree is 90.00 and the radian is 1.57\n"
+        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as stdout:
+            c.print()
+        self.assertEqual(solve_text, stdout.getvalue())
 
 
     def test_a7_ex2(self):
@@ -39,57 +41,63 @@ class test_Assignment_7(unittest.TestCase):
             matrix1 = deepcopy(matrix)
             c1 = Rotate(matrix1, 90)
             c1.degree
-            c11 = c1.rotation()
-            self.assertEqual(rotate90, c11)
-            self.assertEqual(matrix, matrix1)
-
-            matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-            rotatem90 = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-            matrix1 = deepcopy(matrix)
-            c2 = Rotate(matrix1, -90)
-            c22 = c2.rotation()
-            self.assertEqual(rotatem90, c22)
-            self.assertEqual(matrix, matrix1)
-
-            matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
-                      [21, 22, 23, 24, 25]]
-            rotate180 = [[25, 24, 23, 22, 21], [20, 19, 18, 17, 16], [15, 14, 13, 12, 11], [10, 9, 8, 7, 6],
-                         [5, 4, 3, 2, 1]]
-            matrix1 = deepcopy(matrix)
-            c3 = Rotate(matrix1, 180)
-            c33 = c3.rotation()
-            self.assertEqual(rotate180, c33)
-            self.assertEqual(matrix, matrix1)
-
-            matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-            rotatem90 = [[3, 6, 9], [2, 5, 8], [1, 4, 7]]
-            matrix1 = deepcopy(matrix)
-            c4 = Rotate(matrix1, 90, True)
-            c44 = c4.rotation()
-            self.assertIsNone(c44)
-            self.assertEqual(rotatem90, matrix1)
-
-            matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-            rotatem90 = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-            matrix1 = deepcopy(matrix)
-            c5 = Rotate(matrix1, -90, True)
-            c55 = c5.rotation()
-            self.assertIsNone(c55)
-            self.assertEqual(rotatem90, matrix1)
-
-            matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
-                      [21, 22, 23, 24, 25]]
-            rotate180 = [[25, 24, 23, 22, 21], [20, 19, 18, 17, 16], [15, 14, 13, 12, 11], [10, 9, 8, 7, 6],
-                         [5, 4, 3, 2, 1]]
-            matrix1 = deepcopy(matrix)
-            c6 = Rotate(matrix1, 180, inplace=True)
-            c66 = c6.rotation()
-            self.assertIsNone(c66)
-            self.assertEqual(rotate180, matrix1)
-            pass
         except (AttributeError, ImportError, ModuleNotFoundError) as er:
             warnings.warn("You need to put the a7_ex2.py in the same directory",ImportWarning, source=er)
             self.skipTest("Possible not implemented")
+
+        from a7_ex2 import Rotate
+        matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        rotate90 = [[3, 6, 9], [2, 5, 8], [1, 4, 7]]
+        # Rotate 90deg
+        matrix1 = deepcopy(matrix)
+        c1 = Rotate(matrix1, 90)
+        c11 = c1.rotation()
+        self.assertEqual(rotate90, c11)
+        self.assertEqual(matrix, matrix1)
+
+        matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        rotatem90 = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+        matrix1 = deepcopy(matrix)
+        c2 = Rotate(matrix1, -90)
+        c22 = c2.rotation()
+        self.assertEqual(rotatem90, c22)
+        self.assertEqual(matrix, matrix1)
+
+        matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+                  [21, 22, 23, 24, 25]]
+        rotate180 = [[25, 24, 23, 22, 21], [20, 19, 18, 17, 16], [15, 14, 13, 12, 11], [10, 9, 8, 7, 6],
+                     [5, 4, 3, 2, 1]]
+        matrix1 = deepcopy(matrix)
+        c3 = Rotate(matrix1, 180)
+        c33 = c3.rotation()
+        self.assertEqual(rotate180, c33)
+        self.assertEqual(matrix, matrix1)
+
+        matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        rotatem90 = [[3, 6, 9], [2, 5, 8], [1, 4, 7]]
+        matrix1 = deepcopy(matrix)
+        c4 = Rotate(matrix1, 90, True)
+        c44 = c4.rotation()
+        self.assertIsNone(c44)
+        self.assertEqual(rotatem90, matrix1)
+
+        matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        rotatem90 = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+        matrix1 = deepcopy(matrix)
+        c5 = Rotate(matrix1, -90, True)
+        c55 = c5.rotation()
+        self.assertIsNone(c55)
+        self.assertEqual(rotatem90, matrix1)
+
+        matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+                  [21, 22, 23, 24, 25]]
+        rotate180 = [[25, 24, 23, 22, 21], [20, 19, 18, 17, 16], [15, 14, 13, 12, 11], [10, 9, 8, 7, 6],
+                     [5, 4, 3, 2, 1]]
+        matrix1 = deepcopy(matrix)
+        c6 = Rotate(matrix1, 180, inplace=True)
+        c66 = c6.rotation()
+        self.assertIsNone(c66)
+        self.assertEqual(rotate180, matrix1)
 
 
     def test_a7_ex3(self):
@@ -97,12 +105,14 @@ class test_Assignment_7(unittest.TestCase):
             from a7_ex3 import Distance
             x = Distance(10)
             x.x
-            self.assertEqual("Distance: the number of vectors =10", x.to_string())
-            with self.assertRaises(NotImplementedError):
-                x.dist()
         except (AttributeError, ImportError, ModuleNotFoundError) as er:
             warnings.warn("You need to put the a7_ex3.py in the same directory",ImportWarning, source=er)
             self.skipTest("Possible not implemented")
+        from a7_ex3 import Distance
+        x = Distance(10)
+        self.assertEqual("Distance: the number of vectors =10", x.to_string())
+        with self.assertRaises(NotImplementedError):
+            x.dist()
 
 
     def test_a7_ex4(self):
@@ -112,13 +122,17 @@ class test_Assignment_7(unittest.TestCase):
             vect2 = [4, 5, 6]
             k = Minkowski(2, vect1, vect2)
             k.x
-            self.assertEqual("Minkowski: the number of vectors =2, vector_1=[1, 2, 3], vector_2=[4, 5, 6]",
-                             k.to_string())
-            self.assertEqual(5.1962, k.dist())
-
         except (AttributeError, ImportError, ModuleNotFoundError) as er:
             warnings.warn("You need to put the a7_ex4.py in the same directory",ImportWarning, source=er)
             self.skipTest("Possible not implemented")
+
+        from a7_ex4 import Minkowski
+        vect1 = [1, 2, 3]
+        vect2 = [4, 5, 6]
+        k = Minkowski(2, vect1, vect2)
+        self.assertEqual("Minkowski: the number of vectors =2, vector_1=[1, 2, 3], vector_2=[4, 5, 6]",
+                         k.to_string())
+        self.assertEqual(5.1962, k.dist())
 
 
     def test_a7_ex5(self):
@@ -128,13 +142,17 @@ class test_Assignment_7(unittest.TestCase):
             vect2 = [4, 5, 6]
             m = Manhattan(2, vect1, vect2)
             m.x
-            self.assertEqual("Manhattan: the number of vectors =2, vector_1=[1, 2, 3], vector_2=[4, 5, 6]",
-                             m.to_string())
-            self.assertEqual(9.0, m.dist())
 
         except (AttributeError, ImportError, ModuleNotFoundError) as er:
             warnings.warn("You need to put the a7_ex5.py in the same directory",ImportWarning, source=er)
             self.skipTest("Possible not implemented")
+        from a7_ex5 import Manhattan
+        vect1 = [1, 2, 3]
+        vect2 = [4, 5, 6]
+        m = Manhattan(2, vect1, vect2)
+        self.assertEqual("Manhattan: the number of vectors =2, vector_1=[1, 2, 3], vector_2=[4, 5, 6]",
+                         m.to_string())
+        self.assertEqual(9.0, m.dist())
 
 
     def test_a7_ex6(self):
@@ -144,11 +162,15 @@ class test_Assignment_7(unittest.TestCase):
             vect2 = [4, 5, 6]
             e = Euclidean(2, vect1, vect2)
             e.x
-            self.assertEqual("Euclidean: the number of vectors =2, vector_1=[1, 2, 3], vector_2=[4, 5, 6]", e.to_string())
-            self.assertEqual(5.1962, e.dist())
         except (AttributeError, ImportError, ModuleNotFoundError) as er:
             warnings.warn("You need to put the a7_ex6.py in the same directory",ImportWarning, source=er)
             self.skipTest("Possible not implemented")
+        from a7_ex6 import Euclidean
+        vect1 = [1, 2, 3]
+        vect2 = [4, 5, 6]
+        e = Euclidean(2, vect1, vect2)
+        self.assertEqual("Euclidean: the number of vectors =2, vector_1=[1, 2, 3], vector_2=[4, 5, 6]", e.to_string())
+        self.assertEqual(5.1962, e.dist())
 
 
 if __name__ == '__main__':
---
+++ Assignment_9/test_Assignment_9.py
@@ -0,0 +1,132 @@
+#  ==================================================
+#    * Copyright (c) 2023 Julian Wieser From 2023.12.06 to Present.
+#    * All rights are reserved
+#  ==================================================
+import subprocess
+import sys
+import unittest
+import unittest.mock
+import os
+import warnings
+import io
+from imp import reload
+
+
+class Assignment_9(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_a9_ex1(self):
+        try:
+            with unittest.mock.patch('builtins.input',
+                                     side_effect=["a9_ex1_data.txt", "", "(d..d)", "\\d{3}", "\\d{ab12.c}", "d[^ ]+",
+                                                  ""]):
+                with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as stdout:
+                    __import__("a9_ex1")
+                if stdout.getvalue() == "":
+                    raise FileExistsError
+        except FileExistsError as er:
+            warnings.warn("You need to put the a9_ex1.py in the same directory", ImportWarning, source=er)
+            self.skipTest("Possible not implemented")
+
+        solve_text = "(d..d): ['deed', 'de d']\n" \
+                     "\\d{3}: ['780', '222', '341', '445', '100']\n" \
+                     "\d{ab12.c}: []\n" \
+                     "d[^ ]+: ['d23g780nb', 'deed', 'de', 'ddd32']\n"
+        with unittest.mock.patch('builtins.input',
+                                 side_effect=["a9_ex1_data.txt", "utf-8", "(d..d)", "\\d{3}", "\\d{ab12.c}", "d[^ ]+", ""]):
+            with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as stdout:
+                reload(__import__("a9_ex1"))
+            self.assertEqual(solve_text, stdout.getvalue())
+        with unittest.mock.patch('builtins.input',
+                                 side_effect=["a9_ex2_data.txt"]):
+            with self.assertRaises(ValueError) as e:
+                reload(__import__("a9_ex1"))
+        self.assertEqual("'a9_ex2_data.txt' is not a file", str(e.exception))
+
+    def test_a9_ex2(self):
+        try:
+            from a9_ex2 import extract_emails
+            if extract_emails(""):
+                raise Exception
+        except Exception as er:
+            warnings.warn("You need to put the a9_ex2.py in the same directory", ImportWarning, source=er)
+            self.skipTest("Possible not implemented")
+
+        from a9_ex2 import extract_emails
+        t = """
+        Here are some email addresses:
+        john.doe@example.com, alice_smith123@gmail.com, ABC+@a-b-c.aBc,
+        contact@company.org, and info@sub.domain.co.uk.
+        Some invalid email addresses are:
+        john@, @example.com, user@domain, us/er@email.com,
+        invalid@domain.f and invalid.email@invalid@domain.com.
+        """
+        self.assertEqual(['john.doe@example.com', 'alice_smith123@gmail.com', 'ABC+@a-b-c.aBc',
+                          'contact@company.org', 'info@sub.domain.co.uk'], extract_emails(t))
+
+    def test_a9_ex3(self):
+        try:
+            from a9_ex3 import sum_of_fractions
+            if sum_of_fractions(1, 2) is None:
+                raise Exception
+        except Exception as er:
+            warnings.warn("You need to put the a9_ex3.py in the same directory", ImportWarning, source=er)
+            self.skipTest("Possible not implemented")
+        result = subprocess.run([sys.executable, "a9_ex3.py", "-n 10", "-p 2"], capture_output=True)
+        self.assertAlmostEqual(0.626383161, float(
+            result.stdout.decode().split("Euler-Mascheroni constant approximation (10 terms):")[1]), delta=9)
+        result = subprocess.run([sys.executable, "a9_ex3.py", "-n 1_000_000_000", "-p 4"], capture_output=True)
+        self.assertAlmostEqual(0.577215665, float(
+            result.stdout.decode().split("Euler-Mascheroni constant approximation (1000000000 terms):")[1]), delta=9)
+        pass
+
+    def test_a9_ex4(self):
+        try:
+            result = subprocess.run([sys.executable, "a9_ex4.py", "--args hello"], capture_output=True)
+            if result.stderr.decode().replace("\r", "") == "":
+                raise Exception
+        except Exception as er:
+            warnings.warn("You need to put the a9_ex3.py in the same directory", ImportWarning, source=er)
+            self.skipTest("Possible not implemented")
+        solve_text = "usage: a9_ex4.py [-h] -p PROGRAM [-a [ARGS ...]] [-t TIMEOUT]\n" \
+                     "a9_ex4.py: error: the following arguments are required: -p/--program\n"
+        result = subprocess.run([sys.executable, "a9_ex4.py", "--args hello"], capture_output=True)
+        self.assertEqual(solve_text, result.stderr.decode().replace("\r", ""))
+
+        solve_text = "Running program 'pythn' without any arguments with a 60s timeout\n" \
+                     "The specified program 'pythn' could not be found\n"
+        result = subprocess.run([sys.executable, "a9_ex4.py", '--program', 'pythn'], capture_output=True,
+                                encoding="utf-8")
+        self.assertEqual(solve_text, result.stdout.replace("\r", ""))
+
+        solve_text = "Running program 'python' without any arguments with a 10s timeout\n" \
+                     "The program execution timed out\n"
+        result = subprocess.run([sys.executable, "a9_ex4.py", "-p", "python", "--timeout", "10"], capture_output=True,
+                                encoding="utf-8")
+        self.assertEqual(solve_text, result.stdout.replace("\r", ""))
+
+        solve_text = "Running program 'python' with arguments ['a9_ex3.py'] with a 60s timeout\n" \
+                     "The 'python' finished with exit code 0\n" \
+                     "The 'python' produced the following standard output:\n" \
+                     "Euler-Mascheroni constant approximation (1000 terms): 0.577715582\n"
+        result = subprocess.run([sys.executable, "a9_ex4.py", "-p", "python", "--args", "a9_ex3.py"],
+                                capture_output=True,
+                                encoding="utf-8")
+        self.assertEqual(solve_text, result.stdout.replace("\r", ""))
+
+        solve_text = "Running program 'python' with arguments ['a9_ex3.py', '-x 9'] with a 60s timeout\n" \
+                     "The 'python' finished with exit code 2\n" \
+                     "The 'python' produced the following error output:\n" \
+                     "usage: a9_ex3.py [-h] [-p PROCESSES] [-n N]\n" \
+                     "a9_ex3.py: error: unrecognized arguments: -x 9\n"
+        result = subprocess.run([sys.executable, "a9_ex4.py", "-p", "python", "-a", "a9_ex3.py","-x 9"],
+                                capture_output=True,
+                                encoding="utf-8")
+        self.assertEqual(solve_text, result.stdout.replace("\r", ""))
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
